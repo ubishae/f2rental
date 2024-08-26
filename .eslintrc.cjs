51a8c8634226644522a95ae337a7a646d0ba75da
Bisha <bisha.mail.drop@gmail.com>
2024-08-26 14:10:54 +0400

    chore: configure eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,7 +11,7 @@ const config = {
     "plugin:@typescript-eslint/stylistic-type-checked",
   ],
   rules: {
-    "no-empty-interface": "off",
+    "@typescript-eslint/no-empty-object-type": "off",
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
     "@typescript-eslint/consistent-type-imports": [
